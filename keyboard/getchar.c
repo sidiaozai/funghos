c4705401068d6a0c3cb069683378fe1db14c757e
dlabaljan <dlabaljan@2331e321-2848-0410-bce0-0bb4abde0bdb>
2008-03-18 15:26:25 +0000

    nomsg

--- keyboard/getchar.c
+++ keyboard/getchar.c
@@ -1,13 +1,35 @@
 #include <system.h>
 #include <stdio.h>
 #include "kbmap.h"
+#include "kb.h"
+
 
 char getchar()
 {
-  while (!(inportb(0x64) & 0x1))
-    ;
-  unsigned int scancode=inportb(0x60);
-  char c=kbdus[scancode];
-  putch(c);
-  return c;
+  char c;
+  unsigned int scancode;
+  while (1) {
+     while (!(inportb(0x64) & 0x1))
+      ;
+     scancode = inportb(0x60);
+     
+     if (scancode & 0x80)
+       {
+	 if (scancode==170||scancode==182) kb_flags.shift=FALSE;
+	 /*170 - levy shift, 182 - pravy shift*/
+       }
+     else
+       {
+	 if (kbdus[scancode]!='\S') {
+	   switch (kb_flags.shift) {
+	   case 0:c=kbdus[scancode];putch(c);break;
+	   case 1:c=kbdus2[scancode];putch(c);break;
+	   }
+	 } else {
+	   c='\S';
+	   kb_flags.shift=TRUE;
+	 }
+       }
+     if (c!='\S') return c;
+  }
 }
