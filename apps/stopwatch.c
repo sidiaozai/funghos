7be0d76bd4f7142d1837693723ddf7d9658bf5e6
dlabaljan <dlabaljan@2331e321-2848-0410-bce0-0bb4abde0bdb>
2008-03-20 12:54:27 +0000

    modified stopwatch() function

--- apps/stopwatch.c
+++ apps/stopwatch.c
@@ -1,6 +1,31 @@
+#include <system.h>
 #include <stdio.h>
+#include <apps/timer.h>
+
+
 
 void stopwatch()
 {
-  puts("Stopwatch will be avaible soon...\n");
+  static int running=0;
+  while (1) {
+  if (!running) {
+    mkwin(34,10,46,15,0);
+    txtclr(RED,LIGHT_GREY);
+    puts("Stopwatch\n");
+    txtclr(BLUE,LIGHT_GREY);
+    running=TRUE;
+    timer_hrs=timer_min=timer_sec=timer_ticks=0;
+    puti(0);puti(timer_hrs);putch(':');puti(0);puti(timer_min);putch(':');puti(0);puti(timer_sec);
+  } else {
+    csr_x=rtwin(CSRX)+1;
+    ///    puts("\b\b\b\b\b\b\b\b");
+
+    if (timer_hrs<10) {puti(0);puti(timer_hrs);putch(':');} else {puti(timer_hrs);putch(':');}
+
+    if (timer_min<10) {puti(0);puti(timer_min);putch(':');} else {puti(timer_min);putch(':');}
+
+    if (timer_sec<10) {puti(0);puti(timer_sec);} else {puti(timer_sec);}
+
+  }
+  }
 }
