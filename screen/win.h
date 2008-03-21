f3aaae1268203332cdf2f85456cbd44f107a1e61
dlabaljan <dlabaljan@2331e321-2848-0410-bce0-0bb4abde0bdb>
2008-03-21 18:28:59 +0000

    little improvements

---
+++ screen/win.h
@@ -0,0 +1,21 @@
+#ifndef WIN_H
+#define WIN_H
+
+#define SYSLINE 1 /* border (up) */
+#define WINSIZEX 40  /* default <-X-> */
+#define WINSIZEY 15  /* default <-Y-> */
+#define WINMAX 10 /* how many windows can be opened - linked list later */
+#define BDFOREGROUND 0
+#define BDBACKGROUND BLUE
+#define WINFOREGROUND 0
+#define WINBACKGROUND 7
+#define TXTFOREGROUND 1
+#define TXTBACKGROUND WINBACKGROUND
+#define WINEND 2
+#define WINSCROLL 3
+#define CSRY2 4
+#define CSRX2 5
+/* CSRX & CSRY are defined in system.h */
+
+
+#endif
