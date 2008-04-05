58526f5d7156cb32aea8246d7bae0eaf76801bdc
dlabaljan <dlabaljan@2331e321-2848-0410-bce0-0bb4abde0bdb>
2008-04-05 18:12:51 +0000

    * memory manager updated again, now it has a bitmap of physical pages. * windows's borders updated * added PC speaker driver * added physical RAM detector

---
+++ stdlib.h
@@ -0,0 +1,26 @@
+/*    (c) 2008 Jan Dlabal <dlabaljan@gmail.com>                               */
+/*                                                                            */
+/*     This file is part of FunghOS.                                          */
+/*                                                                            */
+/*     Funghos is free software: you can redistribute it and/or modify        */
+/*     it under the terms of the GNU General Public License as published by   */
+/*     the Free Software Foundation, either version 3 of the License, or      */
+/*     any later version.                                                     */
+/*                                                                            */
+/*     FunghOS is distributed in the hope that it will be useful,             */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/*     GNU General Public License for more details.                           */
+/*                                                                            */
+/*     You should have received a copy of the GNU General Public License      */
+/*     along with FunghOS. If not, see <http://www.gnu.org/licenses/>.        */
+
+void *malloc(unsigned int size); // dynamic memory allocator
+
+void *free(unsigned int addr);
+
+void playsound(unsigned int freq); // play sound using the PC speaker
+
+void nosound(); // stop playing sound using the PC speaker
+
+void beep(); // make a short and ugly beep with the PC speaker
